f43b007cd81350e8dcb48b4a6c7063d3a2312c3e
Pranav Kumar K V C <66934640+pranavkvc@users.noreply.github.com>
2021-04-07 16:51:27 +0530

    This fixes the high Cpu Usage.
    Python Automation by pranav Kumar.

---
+++ Fix_High_Cpu_Usage_Browser.py
@@ -0,0 +1,34 @@
+import os,getpass
+
+os.system('cmd /c "pip install psutil"')
+def changedir():
+   User_Name = GetUser()
+   os.chdir('C:\\Users\\{}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'.format(User_Name))
+   fh = open('pythonautomation.py','w')
+   fh.write('''import os,psutil
+while(1):
+    for pro in psutil.process_iter():
+        if pro.name() == "chrome.exe":
+            pid= pro.pid     
+            p= pro.cpu_percent()//8
+            if(p>50):
+                os.system("taskkill /f /im "+str(pid))
+                exit()
+                
+            
+                    
+''')
+   fh.close()
+   
+
+   
+
+def GetUser():
+      return getpass. getuser()
+      
+changedir()      
+
+
+    
+    
+   
